fdcc9bf4823c46f7fe79babda6691dd2cfab4e55
AnneJTL <anne.chatin@jointheleague.org>
2018-10-03 17:03:59 -0700

    End of Running Race

--- src/Athlete.java
+++ src/Athlete.java
@@ -1,22 +1,39 @@
 public class Athlete {
 
-        static int count;
-        static String raceLocation = "New York";
-        static String raceStartTime = "9.00am";
+    private static int count;
+    private static String raceLocation = "New York";
+    private static String raceStartTime = "9.00am";
 
-        String name;
-        int speed;
-        int bibNumber;
+    private String name;
+    private int speed;
+    private int bibNumber;
 
-        Athlete (String name, int speed){
-            this.name = name;
-            this.speed = speed;
-        }
+    Athlete(String name, int speed) {
+        this.name = name;
+        this.speed = speed;
+        count++;
+        this.bibNumber = count;
+    }
 
-        public static void main(String[] args) {
-            //create two athletes
-            Athlete athlete1 = new Athlete("First Athlete", 50) ;
-            // print their names, bibNumbers, and the location of their race. }
-        }
+    public static void setRaceLocation(String newLocation) {
+        raceLocation = newLocation;
+    }
+
+    public static void main(String[] args) {
+        //create two athletes
+        Athlete athlete1 = new Athlete("First Athlete", 50);
+        Athlete athlete2 = new Athlete("Second Athlete", 45);
+
+        // print their names, bibNumbers, and the location of their race. }
+        System.out.println("Name: " + athlete1.name + " - bibNumber: " + athlete1.bibNumber + " - Location: " + raceLocation);
+        System.out.println("Name: " + athlete2.name + " - bibNumber: " + athlete2.bibNumber + " - Location: " + raceLocation);
+        System.out.println("Number of participants: " + count);
+
+        // After changing the location
+        setRaceLocation("San Diego");
+        System.out.println("Name: " + athlete1.name + " - bibNumber: " + athlete1.bibNumber + " - Location: " + raceLocation);
+        System.out.println("Name: " + athlete2.name + " - bibNumber: " + athlete2.bibNumber + " - Location: " + raceLocation);
+        System.out.println("Number of participants: " + count);
+    }
 
 }
